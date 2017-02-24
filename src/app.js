789b232744c58cce41a1e71fb7779b1066966caa
mayacger <mayasence@gmail.com>
2017-02-24 10:46:48 +0800

    NavBar
    NavBar

--- src/app.js
+++ src/app.js
@@ -7,16 +7,30 @@ import {  Link } from 'react-router'
 
 
 class App extends Component{
+
+  goBack = () => {
+    console.log(this.context.router)
+    console.log(this.props)
+    const { location } = this.props;
+    if (location.pathname !== '/') {
+      this.context.router.goBack()
+    }
+  }
+
   render() {
     const { routes, location, children } = this.props;
     return (
       <div >
-        <NavBar leftContent="返回" mode="light" onLeftClick={() => console.log('onLeftClick')}
-        rightContent={[
-          <Icon key="0" type="search" style={{ marginRight: '0.32rem' }} />,
-          <Icon key="1" type="ellipsis" />,
-        ]}
-      >{routes[routes.length -1].title || '首页'}</NavBar>
+        <NavBar leftContent={location.pathname === '/' ? false : '返回'}
+                iconName={location.pathname === '/' ? false : 'left' }
+                mode="light"
+                onLeftClick={() => this.goBack()}
+                rightContent={[
+                  <Icon key="0" type="search" style={{ marginRight: '0.32rem' }} />,
+                  <Icon key="1" type="ellipsis" />,
+                ]}>
+        {         routes[routes.length -1].title || '首页'}
+      </NavBar>
 
         <ReactCSSTransitionGroup
           component="div"
@@ -33,5 +47,9 @@ class App extends Component{
   }
 
 }
+
+App.contextTypes = {
+  router: React.PropTypes.object
+};
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {  Router, Route, hashHistory, IndexRoute } from 'react-router'
+import {  Router, Route, hashHistory, IndexRoute, browserHistory } from 'react-router'
 
 import App from './app'
 import Counter from './components/Counter';
